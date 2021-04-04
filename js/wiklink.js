a992f28b5e07f9e1578671dca290012961522e77
sinearc <72674341+sinearc@users.noreply.github.com>
2021-04-04 01:09:48 -0600

    testing more js

--- js/wiklink.js
+++ js/wiklink.js
@@ -1,4 +1,29 @@
+class WikiEntry {
+	constructor(link, ...topics) {
+		this.link = link
+		this.topics = topics
+		this.title = link.split('/').pop().replace(/_/g, ' ')
+	}
+	forceTitle(customTitle) {
+		this.title = customTitle
+	}
+}
+
 function handleData(data) {
+	let outHTML = []
+	for (datum of data.list) {
+		let buildString = []
+		buildString.push(datum.title)
+		buildString.unshift(`<a href="${datum.link}">`)
+		buildString.push('</a>')
+		for (tag of datum.tags) {
+			buildString.push(`<div>${tag}</div>`)
+		}
+		buildString.unshift('<div>')
+		buildString.push('</div>')
+		outHTML.push(buildString.join(''))
+	}
+	console.log(outHTML.join(''))
 }
 
 const JSONDATA = () => {

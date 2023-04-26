1a84ea615a93212ed2c3fabe589bca23bdb1a40f
jpVinnie <janpaul.ramos@protonmail.com>
2023-04-25 22:50:04 -0400

    change default to light mode

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function change_icon() {
-  if(document.getElementById("mode").src == "https://img.icons8.com/emoji/512/white-circle-emoji.png"){
-    document.getElementById("mode").src = "https://img.icons8.com/emoji/256/black-circle-emoji.png";
-    document.getElementById("mode-sec").style.color = "#e6d2b5";
+  if(document.getElementById("mode").src == "https://img.icons8.com/emoji/256/black-circle-emoji.png"){
+    document.getElementById("mode").src = "https://img.icons8.com/emoji/512/white-circle-emoji.png";
+    document.getElementById("mode-sec").style.color = "#282828";
   }
   else {
-    document.getElementById("mode").src = "https://img.icons8.com/emoji/512/white-circle-emoji.png"
-    document.getElementById("mode-sec").style.color = "#282828";
+    document.getElementById("mode").src = "https://img.icons8.com/emoji/256/black-circle-emoji.png"
+    document.getElementById("mode-sec").style.color = "#e6d2b5";
   }
 }
 
